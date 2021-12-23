cbc45d353e73ad13a0b5a59ff72e102895e06155
NaheenMoh <96561384+NaheenMoh@users.noreply.github.com>
2021-12-23 19:04:40 +0530

    Update Movies.java

--- src/com/prio/Movies.java
+++ src/com/prio/Movies.java
@@ -3,6 +3,7 @@ package com.prio;
 import net.proteanit.sql.DbUtils;
 
 import javax.swing.*;
+import java.awt.*;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
@@ -12,21 +13,31 @@ public class Movies extends JFrame {
     String[][] y=new String[40][2];
 
     Movies(String s) throws SQLException {
+        super("MovieList");
+        setSize(515,550);
+        setLocation(150,100);
+        setLayout(null);
+        getContentPane().setBackground(Color.WHITE);
 
         t1=new JTable(y,x);
         Conn c=new Conn();
         ResultSet rs=c.s.executeQuery(s);
-        t1.setModel(DbUtils.resultSetToTableModel(rs));
+        System.out.println(s);
+        int i=0,j=0;
+        while(rs.next())
+        {
+            y[i][j++]=rs.getString("title");
+            y[i][j++]=rs.getString("year");
+            i++;
+            j=0;
+        }
 
         JScrollPane sp = new JScrollPane(t1);
-        sp.setBounds(0, 100, 700, 650);
+        sp.setBounds(0, 20, 500, 550);
         add(sp);
     }
 
     public static void main(String[] args) throws SQLException {
         new Movies("").setVisible(true);
     }
-
-
-
 }
