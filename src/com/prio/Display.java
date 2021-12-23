f410f946e59c8afd13d4777707fe6b227e7c4e45
NaheenMoh <96561384+NaheenMoh@users.noreply.github.com>
2021-12-23 19:02:14 +0530

    Update Display.java

--- src/com/prio/Display.java
+++ src/com/prio/Display.java
@@ -7,29 +7,35 @@ import java.awt.event.ActionListener;
 import java.sql.SQLException;
 
 public class Display extends JFrame implements ActionListener {
-    String id;
-    JTable t1;
+    static int id;
     JLabel l1,l2,l3,l4;
     JButton b1;
-    Display(String str,String id)
+    Display(String str,int id)
     {
         super("Movie Page");
+        this.id=id;
         setLayout(null);
         getContentPane().setBackground(Color.WHITE);
         String[] movies=str.split("}");
 
             String[] arr=movies[0].split(",");
             String[] la=arr[0].split(":");
-            l1=new JLabel(la[1].substring(2));
-            l2=new JLabel(la[2]);
+            if(la[1].equals("\"False\""))
+            {
+                JOptionPane.showMessageDialog(null,"movie Not Found");
 
+            }
+            else {
+                l1 = new JLabel(la[1].substring(2));
+                l2 = new JLabel(la[2]);
+            }
             String[] lb=arr[1].split(":");
             l3=new JLabel(lb[0]);
             l4=new JLabel(lb[1]);
 
-        setBounds(400,200,300,300);
+        setBounds(400,200,400,300);
         l1.setBounds(50+50,50,50,20);
-        l2.setBounds(150,50,50,20);
+        l2.setBounds(150,50,150,20);
         l3.setBounds(50+50,100,50,20);
         l4.setBounds(150,100,50,20);
         add(l1);
@@ -47,16 +53,20 @@ public class Display extends JFrame implements ActionListener {
 
 
     public static void main(String[] args) {
-        new Display("","").setVisible(true);
+
+        new Display("",0).setVisible(true);
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         if(e.getSource()==b1){
-            String s="insert into movies values("+l2.getText()+","+l4.getText()+") where id="+id;
+            System.out.println(l2.getText());
+            String s="insert into movies values("+id+","+l2.getText()+","+l4.getText()+");";
             Conn c=new Conn();
             try {
-                c.s.executeQuery(s);
+                c.s.executeUpdate(s);
+                JOptionPane.showMessageDialog(null,"Added Successfully");
+                this.setVisible(false);
             } catch (SQLException throwables) {
                 throwables.printStackTrace();
             }
