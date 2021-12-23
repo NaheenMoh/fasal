b129e4a8f627412d35e31fadebab437c8603b743
NaheenMoh <96561384+NaheenMoh@users.noreply.github.com>
2021-12-23 19:00:47 +0530

    Update Api.java

--- src/com/prio/Api.java
+++ src/com/prio/Api.java
@@ -9,20 +9,18 @@ import java.net.http.HttpResponse;
 
 public class Api extends JFrame {
     String title;
-    String id;
-    Api(String title,String id) throws IOException, InterruptedException {
+    int id;
+    Api(String title,int id) throws IOException, InterruptedException {
         this.id=id;
         this.title=title;
         var url="http://www.omdbapi.com/?s="+title+"&apikey=b95b45ac";
         var request= HttpRequest.newBuilder().GET().uri(URI.create(url)).build();
         var client = HttpClient.newBuilder().build();
         var response=client.send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(response.statusCode());
-        System.out.println(response.body());
-
+        
         new Display(response.body(),id).setVisible(true);
     }
     public static void main(String[] args)throws IOException,InterruptedException {
-        new Api("","").setVisible(true);
+        new Api("",0).setVisible(true);
     }
 }
